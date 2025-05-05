6bb15fbe015b7ff08a65e4ab1a33472d243e9a4a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-05 20:03:37 +0600

    added new file

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+const ErrorPage = () => {
+
+    return (
+        <div>
+            <h1 className="text-4xl">Error 404..!!</h1>
+            <h2 className="text-2xl">The Page Is Not Found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../Header/Navbar";
 
 const Root = () => {
 
     return (
         <div>
-            <h2>Hello</h2>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
