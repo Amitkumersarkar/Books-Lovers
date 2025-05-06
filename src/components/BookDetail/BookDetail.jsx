a9a43b28a918a0ecc99d9e3493b1563d30237d2e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-06 23:47:12 +0600

    added updated file

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -6,10 +6,17 @@ const BookDetail = () => {
 
     const data = useLoaderData();
     const book = data.find(book => book.bookId === id);
-    console.log(data, book, bookId, id);
+    // console.log(data, book, bookId, id);
+    // destructing data
+    const { bookId: currentBookId, image } = book;
+
     return (
-        <div>
+        <div className="my-12">
             <h2>book details : {bookId}</h2>
+            <img className="w-48" src={image} alt="" />
+            <br />
+            <button className="btn btn-outline font-bold bg-blue-400 mr-4 ">Read</button>
+            <button className="btn btn-outline font-bold bg-green-600">WishList</button>
         </div>
     );
 };
