c189ab9a3497e53af1b0f80a4d94230c7fb5572e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-07 01:43:44 +0600

    added new file

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utility/addToDB";
 
 const BookDetail = () => {
     const { bookId } = useParams();
@@ -8,14 +9,16 @@ const BookDetail = () => {
     const book = data.find(book => book.bookId === id);
     // console.log(data, book, bookId, id);
     // destructing data
-    const { bookId: currentBookId, image } = book;
-
+    const { bookId: currentId, image } = book;
+    const handleAddToRead = (id) => {
+        addToStoredReadList(id);
+    }
     return (
         <div className="my-12">
             <h2>book details : {bookId}</h2>
             <img className="w-48" src={image} alt="" />
             <br />
-            <button className="btn btn-outline font-bold bg-blue-400 mr-4 ">Read</button>
+            <button onClick={() => handleAddToRead(bookId)} className="btn btn-outline font-bold bg-blue-400 mr-4 ">Read</button>
             <button className="btn btn-outline font-bold bg-green-600">WishList</button>
         </div>
     );
